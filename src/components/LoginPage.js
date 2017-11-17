45ed647cb7e953f6d6863e5a9ea48b9a8e364a5d
Pablo Pesce <papesce@gmail.com>
2017-11-17 15:22:12 -0300

    Render of login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,18 +1,71 @@
 import React, { Component } from "react";
 import AccountBox from 'material-ui-icons/AccountBox'
 import TopAppBar from './TopAppBar'
+import Card, { CardActions, CardContent, CardMedia } from 'material-ui/Card';
+import Button from 'material-ui/Button';
+import Typography from 'material-ui/Typography';
+import { withStyles } from 'material-ui/styles';
+import logo from '../spotify-logo.png';
 
+const styles = theme => ({
+    card: {
+        display: 'flex',
+        margin: theme.spacing.unit,
+      },
+      details: {
+        display: 'flex',
+        flexDirection: 'column',
+        "max-width": "400px"
+      },
+      content: {
+        flex: '1 0 auto',
+      },
+      controls: {
+        display: 'flex',
+        alignItems: 'center',
+        paddingLeft: theme.spacing.unit,
+        paddingBottom: theme.spacing.unit,
+      },
+      cover: {
+        marginLeft: 5* theme.spacing.unit,
+        marginRight:  2 * theme.spacing.unit,
+        marginTop: 4 *  theme.spacing.unit,
+        width: 50,
+        height: 50,
+      },
+})
 
 class LoginPage extends Component {
   render() {
+    const { classes, theme } = this.props;
     return (
       <div >
          <TopAppBar/>
-        <p>login page</p>
-        <AccountBox /> 
+         
+         <Card className={classes.card}>
+         <CardMedia
+          className={classes.cover}
+          image={logo}
+          title="Live from space album cover"
+        />
+         <div className={classes.details}>
+         <CardContent className={classes.content}>
+                <Typography type="body1">
+                    To use this app you need to have an Spotify account. Please
+                    use the button below to request access to the Spotify API.
+                </Typography> 
+             </CardContent>
+             <div className={classes.controls}>
+             <CardActions>
+                <Button dense>Login to Spotify</Button>
+           </CardActions>
+
+           </div>
+        </div>
+        </Card> 
       </div>
     );
   }
 }
 
-export default LoginPage
+export default withStyles(styles, { withTheme: true })(LoginPage)
